cb8a1565856ce87a93f31824d22a6b7444e22f67
FyslAhmd <info.faysal.32@gmail.com>
2026-07-01 00:31:23 +0600

    feat: implement useNewProducts hook and refactor NewProductsSection for improved data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -25,7 +25,7 @@ export default function HomePage() {
         <CategoriesSection />
         {/* <FlashSaleSection /> */}
         <FreeDeliveryBanner />
-        <TrendingSection />
+        {/* <TrendingSection /> */}
         <NewProductsSection />
         <CategoryProductsSections />
         <YoutubeVideoSection />
